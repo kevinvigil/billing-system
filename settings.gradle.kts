5bb790ae338a669ff20f9fc9409c28afa53193a4
Kevin Vigil <kevinvigil27@gmail.com>
2024-10-31 19:43:00 -0300

    add more microservices and order

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,3 @@
 rootProject.name = "billingsystem"
 
-include("main-app")
-
-//dependencyResolutionManagement {
-//    versionCatalogs {
-//        create("libs") {
-//            from(files("../gradle/libs.versions.toml"))
-//        }
-//    }
-//}
+include("main-app", "eureka-service", "auth-service", "gateway")
